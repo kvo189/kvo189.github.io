dc80611e3b0c053c6eb664843a8a4543b213e601
Khang Vo <kvo189@gmail.com>
2023-04-26 21:05:00 -0500

    Updates

---
+++ precache-manifest.8ac1c2a89201f07441670253f7e9b67f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa293bcff0498246e380dd6300be4b37",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0eb3bd65a3f0187e5ba9",
+    "url": "/static/css/2.58570433.chunk.css"
+  },
+  {
+    "revision": "e5bd6bdde7f6668289c5",
+    "url": "/static/css/main.ca6232c1.chunk.css"
+  },
+  {
+    "revision": "0eb3bd65a3f0187e5ba9",
+    "url": "/static/js/2.b3d37cc1.chunk.js"
+  },
+  {
+    "revision": "b9889443834e9d715f4aa10893193648",
+    "url": "/static/js/2.b3d37cc1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5bd6bdde7f6668289c5",
+    "url": "/static/js/main.8af0c103.chunk.js"
+  },
+  {
+    "revision": "79f17811c1970678fb17",
+    "url": "/static/js/runtime-main.95455954.js"
+  },
+  {
+    "revision": "968e484128a0b2e20c15f6e0752cddb0",
+    "url": "/static/media/CalculatorApp.968e4841.png"
+  },
+  {
+    "revision": "970066877e23f18be031b6f076e1d0ea",
+    "url": "/static/media/NotesApp.97006687.png"
+  },
+  {
+    "revision": "c912ea819d3434917c655c94120466bf",
+    "url": "/static/media/Project_Saigon_IMG.c912ea81.png"
+  },
+  {
+    "revision": "7f8e612c5bb7003ef27be5e4ab623f6c",
+    "url": "/static/media/TicTacToe.7f8e612c.png"
+  }
+]);
